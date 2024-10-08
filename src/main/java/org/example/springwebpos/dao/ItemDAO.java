f0106f840ec1069d527448cf80302cab2f4a40a0
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-10-08 16:48:01 +0530

    generate random item code

--- src/main/java/org/example/springwebpos/dao/ItemDAO.java
+++ src/main/java/org/example/springwebpos/dao/ItemDAO.java
@@ -2,6 +2,8 @@ package org.example.springwebpos.dao;
 
 import org.example.springwebpos.entity.ItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemDAO extends JpaRepository<ItemEntity, String> {
 }
--- src/main/java/org/example/springwebpos/util/AppUtil.java
+++ src/main/java/org/example/springwebpos/util/AppUtil.java
@@ -10,6 +10,9 @@ public class AppUtil {
     public static String createCustomerId(){
         return "CUSTOMER-"+UUID.randomUUID();
     }
+    public static String createItemCode(){
+        return "ITEM-"+UUID.randomUUID();
+    }
     public  static String toBase64ProfilePic(byte[] profilePic) {
         return Base64.getEncoder().encodeToString(profilePic);
     }
