c9742efe86ba495226f7d36f010655be65373060
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-10-11 18:47:33 +0530

    change annotation for place order

--- src/main/java/org/example/springwebpos/controller/OrderController.java
+++ src/main/java/org/example/springwebpos/controller/OrderController.java
@@ -3,6 +3,7 @@ package org.example.springwebpos.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.springwebpos.dto.OrderDTO;
 import org.example.springwebpos.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
+    @Autowired
     private final OrderService orderService;
 
     @PostMapping
--- src/main/java/org/example/springwebpos/service/OderServiceIMPL.java
+++ src/main/java/org/example/springwebpos/service/OderServiceIMPL.java
@@ -11,6 +11,7 @@ import org.example.springwebpos.entity.OrderDetailEntity;
 import org.example.springwebpos.entity.OrderEntity;
 import org.example.springwebpos.util.AppUtil;
 import org.example.springwebpos.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,9 +22,13 @@ import java.util.stream.Collectors;
 @Transactional
 @RequiredArgsConstructor
 public class OderServiceIMPL implements OrderService {
+    @Autowired
     private final OrderDAO orderDAO;
+    @Autowired
     private final CustomerDAO customerDAO;
+    @Autowired
     private final ItemDAO itemDAO;
+    @Autowired
     private final Mapping mapping;
     @Override
     public OrderDTO saveOrder(OrderDTO orderDTO) {
@@ -50,13 +55,16 @@ public class OderServiceIMPL implements OrderService {
             }
             ItemEntity item = itemOpt.get();
 
+            //check if  sufficient qty is available
             if (item.getQty() < orderDetailDTO.getQuantity()) {
                 throw new RuntimeException("Insufficient quantity for item: " + item.getCode());
             }
 
+            //update item qty
             item.setQty(item.getQty() - orderDetailDTO.getQuantity());
             itemDAO.save(item);
 
+            //set order detail
             orderDetail.setItem(item);
             orderDetail.setQuantity(orderDetailDTO.getQuantity());
             orderDetail.setUnitPrice(orderDetailDTO.getUnitPrice());
