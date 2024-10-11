0ee05d1f51c219ccf4b26a8382940cc120e23897
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-10-11 16:39:56 +0530

    implement Oder & OrderDetail DAO

---
+++ src/main/java/org/example/springwebpos/dao/OderDetailDAO.java
@@ -0,0 +1,9 @@
+package org.example.springwebpos.dao;
+
+import org.example.springwebpos.entity.OrderDetailEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OderDetailDAO extends JpaRepository<OrderDetailEntity,Long> {
+}
